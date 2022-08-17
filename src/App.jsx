106bdeb057c115ed6f4eb1742c2e52a22084def6
Maysa <maysaraquel.ia@gmail.com>
2022-08-17 17:54:02 -0300

    Desafio do terceiro dia

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,29 @@ import './App.css'
 function App() {
   // const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <h1 className="hello">Hello world!</h1>
-      <div id="box-welcome"></div>
-    </div>
-  )
-}
+  const boxWelcome = document.querySelector('.hello');
+  const welcome = document.querySelector('#box-welcome')
 
-
-const boxWelcome = document.querySelector('.hello');
-const welcome = document.querySelector('#box-welcome')
-boxWelcome.onmouseover = function() {
-  welcome.innerHTML = '<h3>Seja bem-vindo à aplicação :)</h3>'
-}
-boxWelcome.onmouseout = function() {
-  welcome.innerHTML = ''
-}
+  boxWelcome.onmouseover = function() {
+    welcome.innerHTML = '<h3>Seja bem-vindo à aplicação :)</h3>'
+  }
+  boxWelcome.onmouseout = function() {
+    welcome.innerHTML = ''
+  }
 
 console.log(boxWelcome)
+
+  return (
+    <div className="App">
+      <h1 className="hello" onmouseover="">Hello world!</h1>
+      <div id="box-welcome"></div>
+    </div>
+    
+  )
+  
+
+}
+
+
 
 export default App
---
+++ src/Button.jsx
@@ -0,0 +1,17 @@
+import './Button.css'
+
+function Button() {
+    return (
+        <div id='main'>
+            <a href='#'>
+                <span></span>
+                <span></span>
+                <span></span>
+                Click me!
+            </a>
+        </div>
+    )
+}
+
+
+export default Button
